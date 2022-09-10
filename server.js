fbc2212e2b7de3f48d0b199275d07516d484d5e6
Zahar <zaharswork@gmail.com>
2022-09-10 19:43:17 +0530

    added env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import Data from "./data.js";
@@ -17,7 +19,8 @@ app.use((req,res,next) => {
 
 
 // DB config
-const connection_url = "mongodb+srv://admin-1:Test123@cluster0.79cyate.mongodb.net/tiktokDB";
+const password = process.env.PASSWORD;
+const connection_url = "mongodb+srv://admin-1:"+password+"@cluster0.79cyate.mongodb.net/tiktokDB";
 
 mongoose.connect(connection_url, {useUnifiedTopology: true});
 
